189032254a17b9bed768070b75829f8f10eacbbe
sukiboo <dereventsov@gmail.com>
2025-08-27 15:39:10 -0400

    move telegram setup

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 from __future__ import annotations
 
-import os
 from datetime import datetime, timezone
 
-import telegram
-
 from src.constants import BOT_NAME
+from src.telegram_bot import TelegramBot
 from src.utils import setup_logger
-
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
 
 logger = setup_logger()
 
@@ -17,29 +12,10 @@ logger = setup_logger()
 class OptionsBot:
     def __init__(self) -> None:
         logger.info(f"{BOT_NAME} initializing...")
-        self.setup_telegram_bot()
-
-    def setup_telegram_bot(self) -> None:
-        if not TELEGRAM_BOT_TOKEN:
-            msg = "TELEGRAM_BOT_TOKEN is not set!"
-            logger.error(msg)
-            raise SystemExit(msg)
-        if not TELEGRAM_CHAT_ID:
-            msg = "TELEGRAM_CHAT_ID is not set!"
-            logger.error(msg)
-            raise SystemExit(msg)
-
-        self.telegram_bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
-        self.send_telegram_message(
+        self.telegram_bot = TelegramBot()
+        self.telegram_bot.send_message(
             msg=f"{BOT_NAME} started at {datetime.now(timezone.utc).isoformat()}"
         )
-
-    def send_telegram_message(self, msg: str) -> None:
-        try:
-            self.telegram_bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=msg)
-            logger.info("[telegram] sent: %s", msg)
-        except Exception as e:
-            logger.error("[telegram] error: %s", e)
 
 
 if __name__ == "__main__":
---
+++ src/telegram_bot.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import logging
+import os
+
+import telegram
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
+
+logger = logging.getLogger()
+
+
+class TelegramBot:
+    def __init__(self) -> None:
+        self.setup()
+
+    def setup(self) -> None:
+        if not TELEGRAM_BOT_TOKEN:
+            msg = "TELEGRAM_BOT_TOKEN is not set!"
+            logger.error(msg)
+            raise SystemExit(msg)
+        if not TELEGRAM_CHAT_ID:
+            msg = "TELEGRAM_CHAT_ID is not set!"
+            logger.error(msg)
+            raise SystemExit(msg)
+
+        self.bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
+        self.chat_id = TELEGRAM_CHAT_ID
+
+    def send_message(self, msg: str) -> None:
+        try:
+            self.bot.send_message(chat_id=self.chat_id, text=msg)
+            logger.info("[telegram] sent: %s", msg)
+        except Exception as e:
+            logger.error("[telegram] error: %s", e)
